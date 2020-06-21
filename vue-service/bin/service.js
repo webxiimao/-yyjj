8ede92729d3b60d2bff057f662aa99fad1b34f95
webxiimao <web_xiimao@outlook.com>
2020-06-21 21:21:17 +0800

    一些基础的索引，配置

--- vue-service/bin/service.js
+++ vue-service/bin/service.js
@@ -2,12 +2,13 @@
  * @Author: maoyuyu
  * @Date: 2020-06-20 16:56:18
  * @LastEditors: maoyuyu
- * @LastEditTime: 2020-06-20 17:36:11
+ * @LastEditTime: 2020-06-21 20:01:52
  * @Description: 
  */ 
 
-const util = require('../libs/utils/util')
+const util = require('vue-service/libs/utils/pathUtil')
 
+// 修改vue.config.js 的检索目录
 process.env.VUE_CLI_SERVICE_CONFIG_PATH = util.getResolvePath('libs', 'config', 'vue.config.js')
 
 require('@vue/cli-service/bin/vue-cli-service')
